2609d1f08ef9c39d944506e5a8ff025b991ff44f
HunghhDev <hunghh.dev@gmail.com>
2019-11-15 15:23:54 +0700

    calling an external api

--- movie-catalog-service/src/main/java/example/springcloud/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/example/springcloud/moviecatalogservice/resource/MovieCatalogResource.java
@@ -29,11 +29,11 @@ public class MovieCatalogResource {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
-        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/"+userId, UserRating.class);
+        UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/"+userId, UserRating.class);
 
         return ratings.getUserRating().stream().map(rating -> {
             // For each movie ID, call movie info service and get details
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
             /*Movie movie = webClientBuilder.build()
                     .get()
                     .uri("http://localhost:8082/movies/" + rating.getMovieId())
@@ -41,7 +41,7 @@ public class MovieCatalogResource {
                     .bodyToMono(Movie.class)
                     .block();*/
             // Put them all together
-            return new CatalogItem(movie.getName(), "test", rating.getRating());
+            return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
         }).collect(Collectors.toList());
     }
 }
